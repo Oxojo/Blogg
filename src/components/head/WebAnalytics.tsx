48ff8ac2e8744669b6e6df7f0dccfb999e63b591
Oxojo <166904252+Oxojo@users.noreply.github.com>
2026-02-15 12:43:14 +0900

    fix: projects

--- src/components/head/WebAnalytics.tsx
+++ src/components/head/WebAnalytics.tsx
@@ -10,9 +10,6 @@ export function WebAnalytics() {
     {
       analytics.google.measurementId && <GoogleAnalytics {...analytics.google} />
     }
-    {
-      analytics.microsoftClarity.projectId && <MicrosoftClarity {...analytics.microsoftClarity} />
-    }
   </>
 }
 
@@ -43,25 +40,6 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', '${measurementId}');`,
-      }}></script>
-    </>
-  )
-}
-
-
-function MicrosoftClarity({
-  projectId,
-}: {
-  projectId: string,
-}) {
-  return (
-    <>
-      <script dangerouslySetInnerHTML={{
-        __html: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "${projectId}");`
       }}></script>
     </>
   )
